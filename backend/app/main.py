8810fca7c5adaafbc73775fbffd5fbe33aa6118f
houimliraed <houimliraed@outlook.fr>
2025-11-15 22:35:16 +0100

    optimizing the response

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,28 @@
 from fastapi import FastAPI
 from app.api.predict import router as predict_router
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
 
-app.include_router(predict_router, prefix="/api")
+
+app = FastAPI(title="AI URL finishing detector",
+              version=1.0,
+              description="API for testing finishing url using ML")
+origins = [
+    "http://localhost:3000",     
+    "http://127.0.0.1:3000",
+    "*"                     
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(predict_router, prefix="/api", tags=["Predict"])
+
+@app.get("/")
+def root():
+    return {"message": "Phishing Detection API is running"}
