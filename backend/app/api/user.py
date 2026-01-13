43d07a228a1ce864a41b4ebf41a39254a01a002b
feditheone <fedi630@gmail.com>
2026-01-13 23:10:56 +0100

    edit userRequest

--- backend/app/api/user.py
+++ backend/app/api/user.py
@@ -3,3 +3,7 @@ from app.schemas.user_schema import UserRequest
 from app.core.user_service import create_user
 
 router = APIRouter()
+
+@router.post("/user")
+def create(request: UserRequest):
+    return create_user(request.email, request.username)
