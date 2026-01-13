727a983b611a891b2659470f0a4c588f5c2c37f0
feditheone <fedi630@gmail.com>
2026-01-13 23:12:44 +0100

    extract creation

---
+++ backend/app/api/extract.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+from app.schemas.extract_schema import ExtractRequest
+from app.core.extractor import extract_data
+
+router = APIRouter()
+
+@router.post("/extract")
+def extract(request: ExtractRequest):
