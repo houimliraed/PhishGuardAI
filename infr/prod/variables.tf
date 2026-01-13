eca8153d42101808ab5aeb70631d22e4e44c084e
Raed Houimli <houimliraed@outlook.fr>
2026-01-13 17:16:16 +0100

    terraform checks

---
+++ infr/prod/variables.tf
@@ -0,0 +1,16 @@
+
+variable "prefix" {
+  description = "Prefix for resource names"
+  default     = "dsoml"
+}
+
+variable "project" {
+  description = "Project name for tagging resources"
+  default     = "DevSecOps-ML"
+}
+
+variable "contact" {
+  description = "Contact information for tagging resources"
+  default     = "houimli@kubelab.io"
+}
+
